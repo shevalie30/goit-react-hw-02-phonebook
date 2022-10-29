0fe355b201a4d81945f8bb4e784498ee186b9690
shevalie30 <103785982+shevalie30@users.noreply.github.com>
2022-10-29 14:39:37 +0300

    .......

--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 // import { nanoid } from 'nanoid';
 import css from './PhoneBook.module.css';
 
@@ -13,23 +14,24 @@ class PhoneBook extends React.Component {
         }
     }
 
-    // model.id = nanoid('name') => {
     // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
     // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    // }
 
     renderList = () => {
         return this.state.contacts.map((nam, index) => <li className={css.item} key={nam + index}>{nam}</li>)
     }
+
     handleInput = e => {
         this.setState({ [e.target.name]: [e.target.value] })
         // this.setState({ name: e.target.value })        
     }
+
     addItem = i => {
         this.setState({ contacts: [...this.state.contacts, i] })
     }
+
     render() {
         return <>
             <div className={css.section}>
@@ -55,4 +57,6 @@ class PhoneBook extends React.Component {
         </>
     }
 }
-export default PhoneBook
+export default PhoneBook;
+
+ PropTypes
